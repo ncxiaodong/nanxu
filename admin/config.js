2d30a78b0f402165a38457a2d86e1ef44568b09c
ncxiaodong <nc_jason@163.com>
2026-05-22 10:46:50 +0800

    添加后台管理系统 - config.js

---
+++ admin/config.js
@@ -0,0 +1,8 @@
+// 后台管理员密码
+const ADMIN_PASSWORD = "hckx-188826";
+
+// API 路由配置
+const API_ROUTES = {
+    stats: 'https://nanxu.vip/api/visits/stats',
+    contacts: 'https://nanxu.vip/api/form'
+};
